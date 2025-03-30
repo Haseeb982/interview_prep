176f23469027a56837a57608c92343eed595eb2a
Haseeb982 <rahfx112@gmail.com>
2025-03-30 21:37:40 +0500

    updating code

--- firebase/client.ts
+++ firebase/client.ts
@@ -4,7 +4,7 @@ import { getAuth } from "firebase/auth";
 import {getFirestore} from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCIp1DpMcdncM12dVMPeB8CqTvBWY6S_Lo",
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_API_KEY,
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID_CLIENT,
     storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
